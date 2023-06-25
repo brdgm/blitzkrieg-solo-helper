77f803e91883becc8341a6c0647a00c8c88ed14f
Stefan Seifert <stefanseifert@users.noreply.github.com>
2023-06-25 12:22:38 +0200

    cosmetic code cleanup

--- src/router/index.ts
+++ src/router/index.ts
@@ -3,7 +3,7 @@ import AppHome from '@/views/AppHome.vue'
 import SetupGame from '@/views/SetupGame.vue'
 import RoundTurn from '@/views/RoundTurn.vue'
 import NotFound from '@/views/NotFound.vue'
-import createRouter from "brdgm-commons/src/util/router/createRouter"
+import createRouter from 'brdgm-commons/src/util/router/createRouter'
 
 const LOCALSTORAGE_KEY = process.env.VUE_APP_LOCALSTORAGE_KEY_PREFIX + "route"
 
