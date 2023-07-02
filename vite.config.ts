e51fbd247cea5fe939c7b0658d36a09293730337
Stefan Seifert <stefanseifert@users.noreply.github.com>
2023-07-02 09:51:37 +0200

    add pwa config

--- vite.config.ts
+++ vite.config.ts
@@ -4,12 +4,44 @@ import vue from '@vitejs/plugin-vue'
 import { VitePWA } from 'vite-plugin-pwa'
 import VueI18nPlugin from '@intlify/unplugin-vue-i18n/vite'
 import path from 'path'
+import { description } from './package.json'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
-    VitePWA(),
+    VitePWA({
+      registerType: 'prompt',
+      includeAssets: [
+        'adaptto-favicon.svg',
+        'favicon.ico',
+        'apple-touch-icon-180x180.png',
+        'maskable-icon-512x512.png'
+      ],
+      manifest: {
+        name: description,
+        short_name: description,
+        description: description,
+        theme_color: '#333',
+        icons: [
+          {
+            src: 'pwa-64x64.png',
+            sizes: '64x64',
+            type: 'image/png'
+          },
+          {
+            src: 'pwa-192x192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          }
+        ]
+      }
+    }),
     VueI18nPlugin({
       include: [path.resolve(__dirname, './src/locales/**')],
       strictMessage: false
