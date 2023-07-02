a07ec62714bfe586a7f13efcaf10668d8d4a615f
Stefan Seifert <stefanseifert@users.noreply.github.com>
2023-07-02 12:39:48 +0200

    asset paths

--- vite.config.ts
+++ vite.config.ts
@@ -16,29 +16,35 @@ export default defineConfig({
       manifestFilename: 'manifest.json',
       filename: 'service-worker.js',
       includeAssets: [
-        'adaptto-favicon.svg',
         'favicon.ico',
-        'img/apple-touch-icon-180x180.png',
-        'img/maskable-icon-512x512.png'
+        'img/icons/favicon.ico',
+        'img/icons/favicon.svg',
+        'img/icons/apple-touch-icon-60x60.png',
+        'img/icons/apple-touch-icon-76x76.png',
+        'img/icons/apple-touch-icon-120x120.png',
+        'img/icons/apple-touch-icon-152x152.png',
+        'img/icons/apple-touch-icon-180x180.png',
+        'img/icons/maskable-icon-512x512.png'
       ],
       manifest: {
         name: description,
         short_name: description,
         description: description,
         theme_color: '#333',
+        background_color: '#000',
         icons: [
           {
-            src: 'img/pwa-64x64.png',
+            src: 'img/icons/pwa-64x64.png',
             sizes: '64x64',
             type: 'image/png'
           },
           {
-            src: 'img/pwa-192x192.png',
+            src: 'img/icons/pwa-192x192.png',
             sizes: '192x192',
             type: 'image/png'
           },
           {
-            src: 'img/pwa-512x512.png',
+            src: 'img/icons/pwa-512x512.png',
             sizes: '512x512',
             type: 'image/png'
           }
