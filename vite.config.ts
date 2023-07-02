7aa75ec06d3046820521f497a885a3736138b5b3
Stefan Seifert <stefanseifert@users.noreply.github.com>
2023-07-02 12:53:07 +0200

    define deploy name in package.json

--- vite.config.ts
+++ vite.config.ts
@@ -4,11 +4,11 @@ import vue from '@vitejs/plugin-vue'
 import { VitePWA } from 'vite-plugin-pwa'
 import VueI18nPlugin from '@intlify/unplugin-vue-i18n/vite'
 import path from 'path'
-import { description } from './package.json'
+import { description, appDeployName } from './package.json'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/blitzkrieg/',
+  base: `/${appDeployName}/`,
   plugins: [
     vue(),
     VitePWA({
