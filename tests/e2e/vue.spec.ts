794c12b8df122efc996cbdbbad1ef7cf92a77c71
Stefan Seifert <stefanseifert@users.noreply.github.com>
2025-03-19 21:34:49 +0100

    fix imports

--- tests/e2e/vue.spec.ts
+++ tests/e2e/vue.spec.ts
@@ -1,25 +1,25 @@
-import { test, expect } from '@playwright/test';
+import { test, expect } from '@playwright/test'
 
 test('smoke test', async ({ page }) => {
-  await page.goto('/');
+  await page.goto('/')
 
   // app home
-  await expect(page.locator('h1')).toHaveText('Blitzkrieg! Solo Helper');
-  await page.getByRole('link', { name: 'Play Game' }).click();
+  await expect(page.locator('h1')).toHaveText('Blitzkrieg! Solo Helper')
+  await page.getByRole('link', { name: 'Play Game' }).click()
 
   // setup game
-  await expect(page.locator('h1')).toHaveText('Setup Game');
-  await page.getByRole('button', { name: 'Start Game' }).click();
+  await expect(page.locator('h1')).toHaveText('Setup Game')
+  await page.getByRole('button', { name: 'Start Game' }).click()
 
   // play three rounds
-  await page.getByRole('link', { name: 'Next Round' }).click();
-  await page.getByRole('link', { name: 'Next Round' }).click();
-  await page.getByRole('link', { name: 'Next Round' }).click();
+  await page.getByRole('link', { name: 'Next Round' }).click()
+  await page.getByRole('link', { name: 'Next Round' }).click()
+  await page.getByRole('link', { name: 'Next Round' }).click()
 
   // finish game
-  await page.getByRole('button', { name: 'Finish Game' }).click();
-  await page.locator('#endGameModal').getByRole('button', { name: 'Finish Game' }).click();
+  await page.getByRole('button', { name: 'Finish Game' }).click()
+  await page.locator('#endGameModal').getByRole('button', { name: 'Finish Game' }).click()
 
   // app home
-  await expect(page.locator('h1')).toHaveText('Blitzkrieg! Solo Helper');
+  await expect(page.locator('h1')).toHaveText('Blitzkrieg! Solo Helper')
 })
